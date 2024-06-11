f66aa5c7379d470442f1dc13638f24c08194a6e6
yumi <yumy19990628@gmail.com>
2024-06-11 17:58:03 +0800

    add - create project & basic page

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import Links from "./links/Links";
+import styles from "./navbar.module.css";
+export default function Navbar() {
+	return (
+		<div className={styles.container}>
+			<div className={styles.logo}>logo</div>
+			<div>
+				<Links />
+			</div>
+		</div>
+	);
+}
---
+++ src/components/navbar/links/navLink/navLink.jsx
@@ -0,0 +1,19 @@
+"use client";
+import Link from "next/link";
+import styles from "./navLink.module.css";
+import { usePathname } from "next/navigation";
+
+export default function NavLink({ item }) {
+	const pathName = usePathname();
+
+	return (
+		<Link
+			href={item.path}
+			className={`${styles.container} ${
+				pathName === item.path && styles.active
+			}`}
+		>
+			{item.title}
+		</Link>
+	);
+}
