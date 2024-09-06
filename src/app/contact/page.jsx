75e8ed2639b350378f87d3d60f1f088853c9fb9e
yumi <yumy19990628@gmail.com>
2024-09-06 11:36:01 +0800

    add - page button

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,25 +2,26 @@ import styles from "./contact.module.css";
 import Image from "next/image";
 
 export default function Contact() {
-	return (
-		<div className={styles.container}>
-			<div className={styles.imgContainer}>
-				<Image src="/contact.png" alt="" fill />
-			</div>
-			<div className={styles.formContainer}>
-				<form action="" className={styles.form}>
-					<input type="text" placeholder="Name and Surname"></input>
-					<input type="text" placeholder="Email Address"></input>
-					<input type="text" placeholder="Phone Number (Optional) "></input>
-					<textarea
-						name=""
-						id=""
-						cols="30"
-						rows="10"
-						placeholder="Message"
-					></textarea>
-				</form>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" alt="" fill />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name and Surname"></input>
+          <input type="text" placeholder="Email Address"></input>
+          <input type="text" placeholder="Phone Number (Optional) "></input>
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Message"
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 }
